6b41129692f290ffb136fc81b69bc896b86b1bb2
Doritzo <81179310+Doritzo@users.noreply.github.com>
2021-04-09 16:36:50 -0500

    Add files via upload

--- html/main.js
+++ html/main.js
@@ -1,13 +1,6 @@
 // sidebar toggle
 const btnToggle = document.querySelector('.toggle-btn');
 
-// Demo Event
-btnToggle.addEventListener('click', function(){
-  console.log('Clicked')
-})
-
-//Transition
-
 function action () {
   console.log('clik')
   document.getElementById('nav').classList.toggle('active');
